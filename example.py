971185071d529e317eb4de764699bb52f73151ab
Bill Chosiad <bill@chosiad.com>
2020-05-10 23:19:35 -0400

    Enhanced example to show invalid reading rate

--- example.py
+++ example.py
@@ -11,16 +11,21 @@ GPIO.setmode(GPIO.BCM)
 instance = DHTXX(pin=16, sensorType=DHTXX.DHT22, scale=DHTXX.FAHRENHEIT)
 
 try:
+	runs = 0
+	errors = 0
 	while True:
+		runs = runs + 1
 		try:
 			result = instance.read()
 			if result.is_valid():
-				print("Last valid input: " + str(datetime.datetime.now()))
+				print("       Time: " + str(datetime.datetime.now()))
 				print("Temperature: %-3.1f F" % result.temperature)
-				print("Humidity: %-3.1f %%" % result.humidity)
+				print("   Humidity: %-3.1f %%" % result.humidity)
 				print("----------------------------------")
 			else:
-				print("invald result")
+				errors = errors + 1
+				rate = float(errors/runs) * 100.0
+				print("Invalid result. Error rate: %-3.1f %%" % rate)
 				print("----------------------------------")
 		except:
 			raise()
